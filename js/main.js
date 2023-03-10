a9c11a6b2ed585ce0ff306b0f3386658f35a12b5
AdamBenDev <adamaya9732@gmail.com>
2023-03-10 09:39:51 +0100

    style and program fix

--- js/main.js
+++ js/main.js
@@ -1,28 +1,83 @@
-//! definisce il prezzo per km
+// memorizzare nome del passeggero, Km & fascia età in una variabile
+var UserName, KmUser, Eta, ButtonGen, ButtonNull;
 
-const prezzoPerKm = 0.21;
-let kmDaPercorrere = prompt("Inserisci il numero di chilometri da percorrere:");
-let etaPasseggero = prompt("Inserisci l'età del passeggero:");
+// quando l'utente clicca su genera memorizza il valore in una variabile
+var ButtonGen = document.getElementById("buttongen");
+
+var ShowBox = document.getElementById("Box_basso");
+ShowBox.classList.add("hidden");
+ShowBox.classList.remove("show");
+
+// aggiungi la funzione di ascolto al click del bottone ed associa i valori presi dalle variabili associate agli input dei diversi campi (nome, Km della destinazione & sconti in base all'età)
+ButtonGen.addEventListener("click", function(){
+
+  var UserName = document.getElementById('InputName');
+  var InputName = UserName.value
+  console.log(InputName);
+
+  var KmUser = document.getElementById("InputKm");
+  var FinalDest = parseInt(KmUser.value);
+  console.log(FinalDest);
 
 
-//! prezzo totale del calcolo 
+  var Eta = document.getElementById("InputTicketSel");
+  var Age = Eta.value;
+  console.log(Age);
 
-let prezzoTotale = kmDaPercorrere * prezzoPerKm;
+  var Offer = "Tariffa Standard"
+  var KmAvPrice = 0.21;
 
+  if (Age == "Maggiorenne") {
 
-
-//! applica lo sconto del 20% per i minorenni
-
-
-if (etaPasseggero < 18) {  
-    prezzoTotale *= 0.8;
-
-//! applica lo sconto del 40% per gli over 65
-  } else if (etaPasseggero >= 65) {  
-    prezzoTotale *= 0.6;
+    var NormalTicket = Math.round(KmAvPrice * FinalDest * 0.80);
+    console.log(NormalTicket);
+    Offer = "Tariffa: Viaggiatore Abituale"
+  } else if (Age == "Minorenne") {
+    var NormalTicket = Math.round(KmAvPrice * FinalDest * 0.60);
+    Offer = "Tariffa: Over 65"
+  } else {
+    var NormalTicket = Math.round(KmAvPrice * FinalDest);
   }
 
+  var Carrozza = Math.floor(Math.random() * 10);
+  var Cp = Math.floor(Math.random() * 10000);
 
-  //! mostra il prezzo totale con due cifre decimali
+  document.getElementById("user_name").innerHTML = InputName;
 
-  alert(`Il prezzo del biglietto è ${prezzoTotale.toFixed(2)} euro.`);  
+  document.getElementById("IdPrenotation").innerHTML = Cp;
+
+  document.getElementById("IdCarrozza").innerHTML = Carrozza;
+
+  document.getElementById("TicketType").innerHTML = Offer;
+
+  document.getElementById("TicketPrice").innerHTML = NormalTicket;
+
+  console.log(NormalTicket);
+  var ShowBox = document.getElementById("Box_basso");
+  ShowBox.classList.add("show");
+  ShowBox.classList.remove("hidden");
+
+}
+);
+
+var ButtonNull = document.getElementById("buttonnull");
+
+ButtonNull.addEventListener("click", function() {
+  document.getElementById("user_name").innerHTML = "";
+
+  document.getElementById("IdPrenotation").innerHTML = "";
+
+  document.getElementById("IdCarrozza").innerHTML = "";
+
+  document.getElementById("TicketType").innerHTML = "";
+
+  document.getElementById('InputName').value = "";
+
+  document.getElementById("InputKm").value = "";
+
+  document.getElementById("InputTicketSel").value = "";
+
+  var ShowBox = document.getElementById("Box_basso");
+  ShowBox.classList.add("hidden");
+  ShowBox.classList.remove("show");
+} );
